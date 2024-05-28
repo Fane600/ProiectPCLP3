174c798d21bcf19173847ab62261711ffd99a16d
Lepadatu Stefan <lepadatustefan67@gmail.com>
2024-05-28 22:32:04 +0300

    Cerinta8

---
+++ ex8.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# Incarca dataset-ul
+file_path = 'train.csv'
+data = pd.read_csv(file_path)
+
+# Completeaza varsta lipsa cu media pasagerilor din aceeasi clasa care au supravietuit
+data['Age'] = data.groupby(['Pclass', 'Survived'])['Age'].transform(lambda x: x.fillna(x.mean()))
+
+# Completeaza valorile lipsa pentru coloanele categorice cu cea mai frecventa valoare
+for column in data.select_dtypes(include=['object']).columns:
+    mode_value = data[column].mode()[0]
+    data[column] = data[column].fillna(mode_value)
+
+# Afiseaza cateva linii pentru a verifica
+print(data.head())
