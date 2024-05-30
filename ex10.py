54eea630bfdab0619e29f93c04b15e6a71040d53
Lepadatu Stefan <lepadatustefan67@gmail.com>
2024-05-30 20:49:55 +0300

    Update Cerinta10

--- ex10.py
+++ ex10.py
@@ -7,11 +7,11 @@ file_path = 'train.csv'
 data = pd.read_csv(file_path)
 
 # Creeaza o coloana pentru a identifica pasagerii singuri (fara rude)
-data['IsAlone'] = (data['SibSp'] == 0) & (data['Parch'] == 0)
+data['Rude'] = (data['SibSp'] == 0) & (data['Parch'] == 0)
 
 # Selecteaza primele 100 de inregistrari
 subset_data = data.head(100)
 
 # Genereaza un catplot pentru a investiga relatia dintre tarif, clasa si supravietuire
-sns.catplot(x='Pclass', y='Fare', hue='Survived', col='IsAlone', kind='strip', data=subset_data, jitter=True)
+sns.catplot(x='Pclass', y='Fare', hue='Survived', col='Rude', kind='strip', data=subset_data, jitter=True)
 plt.show()
