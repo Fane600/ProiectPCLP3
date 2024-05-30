20ba539a8b8f0b328f0be23138d4247f5d6e6245
Lepadatu Stefan <lepadatustefan67@gmail.com>
2024-05-30 21:26:18 +0300

    Update Cerinta8

--- ex8.py
+++ ex8.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Incarca dataset-ul
-file_path = 'train.csv'
+file_path = 'train.csv'  # Modifica aceasta cale daca fisierul este intr-o alta locatie
 data = pd.read_csv(file_path)
 
 # Completeaza varsta lipsa cu media pasagerilor din aceeasi clasa care au supravietuit
@@ -11,6 +11,10 @@ data['Age'] = data.groupby(['Pclass', 'Survived'])['Age'].transform(lambda x: x.
 for column in data.select_dtypes(include=['object']).columns:
     mode_value = data[column].mode()[0]
     data[column] = data[column].fillna(mode_value)
+
+# Salveaza rezultatul intr-un nou fisier CSV
+output_file_path = 'train_filled.csv'
+data.to_csv(output_file_path, index=False)
 
 # Afiseaza cateva linii pentru a verifica
 print(data.head())
